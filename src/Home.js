53d0dd9747c3e5c6ba3607ad525ade65388ed376
Shafi Walsher <shafiwalsher@gmail.com>
2023-09-05 14:13:02 +0530

    update

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,14 @@
-import {useState} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My New Blog', body: 'lorem ipsum.....', author: 'Sam', id: 1},
-        {title: 'Welcome Party!', body: 'lorem ipsum.....', author: 'David', id: 2},
-        {title: 'React Web Application Tips', body: 'lorem ipsum.....', author: 'Sam', id: 3},
-        {title: 'Responsive Web development Tips', body: 'lorem ipsum.....', author: 'SW', id: 4}
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const  {data:blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
 
     return ( 
         <div className="home">
-            <BlogList blogs ={blogs} title="New Blogs" handleDelete = {handleDelete}/>
+            {error && <div> {error} </div>}
+            {isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs ={blogs} title="All Blogs!" />}
         </div>
     );
 }
